7855c8bf6bdaa9e8430b9af0e66bcd82cb2d8d4b
sangwoojun <wjun@mit.edu>
2018-07-12 23:10:33 -0400

    stuff seem to be working

--- examples/simple/simple.cpp
+++ examples/simple/simple.cpp
@@ -37,13 +37,13 @@ int main(int argc, char** argv) {
 
 	printf( "Started!\n" ); fflush(stdout);
 
-	//for ( uint32_t i = 0; i < 1024*1024*32; i++ ) {
+	//for ( uint32_t i = 0; i < 1024*1024*128; i++ ) {
 	for ( uint64_t i = 0; i < (uint64_t)1024*1024*1024*8; i++ ) { //  8*12 GB
-		uint64_t key = 1;//(uint64_t)(rand()&0xffff);
+		uint64_t key = (uint64_t)(rand()&0xffff);
 		//uint64_t key = 1;
 		//while ( !sr->Update(key, (1<<16)|1, false) );
 		while ( !sr->Update(key, 1, false) ) {
-			//printf( "!");
+			//printf( "!" );
 		}
 
 		if ( golden_map.find(key) == golden_map.end() ) {
