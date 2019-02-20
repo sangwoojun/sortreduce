7578b39a20e6232e8a49e008f7fcacf650aa3857
Sang-Woo Jun <swjun@ics.uci.edu>
2019-02-20 00:35:11 -0800

    working on lazyread

--- lazyread/include/LazyRead.hpp
+++ lazyread/include/LazyRead.hpp
@@ -5,13 +5,25 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+// move to config later
+#define WRITEBUF_BYTES 512
+
 class LazyRead {
 public:
-	LazyRead(int target_file_fd, size_t target_buffer_bytes, size_t write_buffer_bytes);
-	void StartTarget();
+	LazyRead(int target_file_fd, size_t target_element_bytes, size_t target_buffer_bytes, size_t write_buffer_bytes);
+	void StartTarget(uint64_t key);
 	void ReadRequest(size_t offset, size_t bytes);
 	void EndTarget();
 private:
+	uint32_t m_target_element_count;
+	void* mp_target_buffer;
+
+	//preallocated circular queue
+	uint32_t* mp_free_target_buffer_q;
+	uint32_t m_free_target_buffer_q_head;
+	uint32_t m_free_target_buffer_q_tail;
+
+	void** mpp_write_buffer;
 };
 
 #endif
--- lazyread/src/LazyRead.cpp
+++ lazyread/src/LazyRead.cpp
@@ -1,15 +1,39 @@
 #include "LazyRead.hpp"
 
-LazyRead::LazyRead(int target_file_fd, size_t target_buffer_bytes, size_t write_buffer_bytes) {
+LazyRead::LazyRead(int target_file_fd, size_t target_element_bytes, size_t target_buffer_bytes, size_t write_buffer_bytes) {
+	size_t element_bytes = target_element_bytes + sizeof(uint32_t)+sizeof(uint64_t); // + count + key
+	size_t element_count = target_element_bytes/element_bytes;
+	mp_target_buffer = malloc(element_bytes*element_count);
+	mp_free_target_buffer_q = (uint32_t*)malloc(element_bytes*sizeof(uint32_t));
+	for ( uint32_t i = 0; i < element_count; i++ ) {
+		mp_free_target_buffer_q[i] = i;
+	}
+	m_free_target_buffer_q_head = 0;
+	m_free_target_buffer_q_tail = element_count;
+	m_target_element_count = element_count;
+
+
+	size_t write_buffer_count = write_buffer_bytes/WRITEBUF_BYTES;
+	mpp_write_buffer = (void**)malloc(sizeof(void*)*write_buffer_count);
+	for ( size_t i = 0; i < write_buffer_count; i++ ) {
+		mpp_write_buffer[i] = malloc(WRITEBUF_BYTES);
+	}
 }
 
 void 
-LazyRead::StartTarget() {
+LazyRead::StartTarget(uint64_t key) {
+	if ( m_free_target_buffer_q_tail+1%m_target_element_count == m_free_target_buffer_q_head ) {
+		//TODO perform reads until empty space exists
+	}
+
+	// Get free target slot
 }
 
 void 
 LazyRead::ReadRequest(size_t offset, size_t bytes) {
-	printf("...");
+	// add count to current target
+	// enqueue to mpp_write_buffer
+	// if write buffer full, write to disk
 }
 	
 void 
